ae0594e996cb2f556dfecc93e5b1ba3def88482e
Yash Raj <76389577+Trickster-cell@users.noreply.github.com>
2024-07-23 22:40:50 +0530

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -56,3 +56,7 @@ async def startup_event():
 @app.get("/")
 async def read_root():
     return {"Trabii Server!!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
