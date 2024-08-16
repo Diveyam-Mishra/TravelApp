a2491e52b6aeff162e995622bc8f76e9f71caa85
Diveyam Mishra <mishradiveyam@gmail.com>
2024-08-16 19:41:17 +0530

    Touch up

--- Routes/EventRoutes.py
+++ Routes/EventRoutes.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException,File, UploadFile, HTTPException, Form
 from Schemas.UserSchemas import SuccessResponse, UserId
 from Schemas.EventSchemas import *
-from Database.Connection import get_db, get_container, get_file_container
+from Database.Connection import *
 from Controllers.Events import create_event, update_event, get_filtered_events, \
-    give_editor_access, get_event_by_id
+    give_editor_access, get_event_by_id,advertise_event
 from sqlalchemy.orm import Session
 from config import JWTBearer
 from Controllers.Auth import get_current_user
 from Models.user_models import User
-from Database.Connection import get_container
 router = APIRouter()
 from typing import List, Dict
 from Controllers.Files import create_event_and_upload_files
@@ -64,7 +63,7 @@ async def edit_event(eventId: str, event_data: EventDetailsupdate, container=Dep
     return await update_event(eventId, event_data, container, current_user)
 
 
-@router.post("/events/filtered/", dependencies=[Depends(JWTBearer())])
+@router.post("/event/filtered/", dependencies=[Depends(JWTBearer())])
 async def filter_events(filters: EventFilter, event_container=Depends(get_container), current_user: User = Depends(get_current_user)):
     if(current_user is None):
         raise HTTPException(status_code=401, detail="Unauthorized")
@@ -73,7 +72,7 @@ async def filter_events(filters: EventFilter, event_container=Depends(get_contai
     result = [{"id": event["id"], "name": event["event_name"], "description": event["event_description"]} for event in events]
     return result
 
-@router.get("/events/details/{eventId}", response_model=SearchEvent)
+@router.get("/event/details/{eventId}", response_model=SearchEvent)
 async def get_event(eventId: str, event_container=Depends(get_container), file_container=Depends(get_file_container)):
     event = await get_event_by_id(eventId, event_container, file_container)
     if event:
@@ -81,7 +80,7 @@ async def get_event(eventId: str, event_container=Depends(get_container), file_c
     else:
         raise HTTPException(status_code=404, detail="Event not found")
 
-@router.post("/events/{eventId}/give-edit-access/", dependencies=[Depends(JWTBearer())],response_model=SuccessResponse)
+@router.post("/event/{eventId}/give-edit-access/", dependencies=[Depends(JWTBearer())],response_model=SuccessResponse)
 async def add_editor(
     eventId: str,  # Add eventId as a path parameter
     userId: UserId,  # Ensure userId is of type int
@@ -90,3 +89,9 @@ async def add_editor(
     current_user: User=Depends(get_current_user)
 ) -> SuccessResponse:
     return await give_editor_access(db, userId.userid, current_user, eventId, container)
+
+
+@router.post("/event/advertise/",response_model=SuccessResponse)
+async def add_advertisement(eventId: takeString,container=Depends(get_container),advertised_events_container=Depends(get_advertisement_container))->SuccessResponse:
+    print("ok")
+    return await advertise_event( eventId, advertised_events_container,container)
