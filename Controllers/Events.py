0744a915e6960eb37f0045d54b347474853fd672
Trickster-cell-Y <76389577+Trickster-cell@users.noreply.github.com>
2024-09-10 04:14:37 +0530

    batch api fix

--- Controllers/Events.py
+++ Controllers/Events.py
@@ -428,7 +428,7 @@ async def advertise_event(event_id: takeString, advertised_events_container, con
     #print("not ok")
     return SuccessResponse(message=f"Event with event_id: {event_id} successfully advertised", success=True)
 
-async def batch_event(event_ids:EventIds, coord:GeoTag,container):
+async def batch_event(event_ids:List[str], coord:GeoTag,container):
     event_ids.eventids = event_ids.eventids[:6]
     query = "SELECT * FROM eventcontainer e WHERE e.id IN ({})".format(
     ", ".join(f"'{event_id}'" for event_id in event_ids.eventids)
